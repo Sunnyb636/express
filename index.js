e59c877cf0aac61e8b60675794ba33e600f8546c
Ubuntu <ubuntu@ip-172-31-5-62.eu-north-1.compute.internal>
2023-03-27 16:27:46 +0000

    modifications to index.js file

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.json("Hello World -- :: This app is deployed for proof of concept for recruitment process")
-"this project is created for demo purpose"})
+  res.json("Hello World -- :: This app is deployed for proof of concept for recruitment process")})
 
 app.listen(3000)
