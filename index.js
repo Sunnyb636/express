3ea8e885d2170438ed19777f8809c5cc528db520
Ubuntu <ubuntu@ip-172-31-5-62.eu-north-1.compute.internal>
2023-03-28 03:59:46 +0000

    modified index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.json("Hello World -- :: This app is deployed for proof of concept for recruitment process")})
+  res.json("Hello World -- :: This app is deployed for proof of concept for recruitment process"
+"deploying nodejs project")})
 
 app.listen(3000)
