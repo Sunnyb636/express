fa45f7470a964b983752a069e054710790e3e0b0
Ubuntu <ubuntu@ip-172-31-5-62.eu-north-1.compute.internal>
2023-03-28 06:08:12 +0000

    modifies index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.json('Hello World')
+  res.json('Hello World -- this project is done for demo purpose')
 })
 
 app.listen(3000)
