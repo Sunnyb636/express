a5e6c6a76ffada583c8b51842e4cca30420ef7fc
Ubuntu <ubuntu@ip-172-31-5-62.eu-north-1.compute.internal>
2023-03-28 06:16:53 +0000

    kubernetes deployment check

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.json('Hello World -- this project is done for demo purpose')
+  res.json('Hello World -- this project is done for demo purpose -Nodejs application deployement on kubernetes cluster ')
 })
 
 app.listen(3000)
