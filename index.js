1c8f94e7f24ce26a8caa2926a510a915e36e2fbe
Ubuntu <ubuntu@ip-172-31-5-62.eu-north-1.compute.internal>
2023-03-28 05:50:11 +0000

    hello

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const app = express()
 
 app.get('/', function (req, res) {
   res.json("Hello World, My name is Sunny Basagare -- :: This app is deployed for proof of concept for recruitment process"
-"deploying nodejs project")})
-
+"deploying nodejs project")}
 app.listen(3000)
